275c2c92396484e8db534b8ad5580017bb92c402
sshwy <jy.cat@qq.com>
2022-06-11 08:47:31 +0800

    feat(utils): add ByteValue

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"crypto/sha256"
+	"fmt"
 )
 
 type HashValue []byte
@@ -13,4 +14,19 @@ func HashSum(a []HashValue) (sum HashValue) {
 	}
 	h.Sum(sum)
 	return
+}
+
+type ByteValue int64
+
+func (r ByteValue) String() string {
+	num := float64(r)
+	if num < 1000 {
+		return fmt.Sprint(int64(num), "B")
+	} else if num < 1e6 {
+		return fmt.Sprintf("%.1f%s", num/1e3, "KB")
+	} else if num < 1e9 {
+		return fmt.Sprintf("%.1f%s", num/1e6, "MB")
+	} else {
+		return fmt.Sprintf("%.1f%s", num/1e9, "GB")
+	}
 }
